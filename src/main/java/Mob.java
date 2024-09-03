4da9873b6004247e23dfbb2c03cca3509d0ec5a0
Adrien Fryson <adrien.fryson.etu@univ-lille.fr>
2024-09-03 10:14:24 +0200

    changes

--- src/main/java/Mob.java
+++ src/main/java/Mob.java
@@ -1,22 +1,16 @@
 package main.java;
 
 public enum Mob {
-    PETIT_DRAGON(50),
-    GRIFFON(50),
-    ORC(50),
-    SERPENT(50),
-    GOBLIN(50),
-    SLIME((50)),
-    SQUELETTE(50),
-    DEMON(50);
-
-    private int pv;
-
-    private Mob(int pv) {
-        this.pv = pv;
-    }
-
-    public int getPv() {return this.pv;}
-
-    public void setPv(int newPv) {this.pv = newPv;}
+    PETIT_DRAGON,
+    GRIFFON,
+    ORC,
+    SERPENT,
+    GOBLIN,
+    SLIME,
+    SQUELETTE,
+    DEMON,
+    GRAND_DRAGON,
+    KRAKEN,
+    SLIME_GEANT,
+    FOURMIE;
 }
