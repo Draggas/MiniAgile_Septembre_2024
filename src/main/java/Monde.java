e92c75ab4e053dab97339609f3f10f1db2d0df55
Baptiste Royer <baptiste.royer.etu@univ-lille.fr>
2024-09-03 10:57:27 +0200

    Ajout des fichiers Case.java et Monde.java

---
+++ src/main/java/Monde.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public enum Monde {
+    MONDE_0(15),
+    MONDE_1(12),
+    MONDE_2(9),
+    MONDE_3(6);
+
+    private int nbCases;
+
+    Monde(int nbCases) {
+        this.nbCases = nbCases;
+    }
+
+    public int getNbCases() {
+        return this.nbCases;
+    }
+}
