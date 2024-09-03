72110bd0b067070b3aba3815bbf027b88e046365
Lucas <draggaspro@gmail.com>
2024-09-03 11:03:41 +0200

    Change Classe.java / Joueur.java

--- src/main/java/Joueur.java
+++ src/main/java/Joueur.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Joueur {
-    String pseudo;
-    int level;
-    int xp;
-    int xpmax;
-    List<Drop> inventory = new ArrayList<Drop>();
-    Classe categorie;
+    private String pseudo;
+    private int level;
+    private int xp;
+    private int xpmax;
+    private List<Drop> inventory = new ArrayList<Drop>();
+    private Classe categorie;
 
-    Joueur(String pseudo, int level, int xp, int xpmax, List<Drop> inventory, Classe categorie){
+    public Joueur(String pseudo, int level, int xp, int xpmax, List<Drop> inventory, Classe categorie){
         this.pseudo = pseudo;
         this.level = level;
         this.xp = xp;
@@ -19,11 +19,11 @@ class Joueur {
         this.categorie = categorie;
     }
 
-    Joueur(String pseudo, Classe categorie){
+    public Joueur(String pseudo, Classe categorie){
         this(pseudo, 1, 0, 10, null, categorie);
     }
 
-    void verifLevel(){
+    public void verifLevel(){
         if(xp >= xpmax){
             if(level == 99){
                 xp = xpmax - 1;
@@ -34,5 +34,68 @@ class Joueur {
             }
 
         }
+    }
+
+    public boolean addInventory(Drop object){
+        return inventory.add(object);
+    }
+
+    public boolean removeInventory(Drop object){
+        return inventory.remove(object);
+    }
+    
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public int getXpmax() {
+        return xpmax;
+    }
+
+    public List<Drop> getInventory() {
+        return inventory;
+    }
+    public int getPv() {
+        return categorie.getPv();
+    }
+
+    public int getAtk() {
+        return categorie.getAtk();
+    }
+
+    public int getDef() {
+        return categorie.getDef();
+    }
+
+    public String getNom() {
+        return categorie.getNom();
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+    public void setPv(int pv) {
+        categorie.setPv(pv);
+    }
+
+    public void setAtk(int atk) {
+        categorie.setAtk(atk);
+    }
+
+    public void setDef(int def) {
+        categorie.setDef(def);
     }
 }
