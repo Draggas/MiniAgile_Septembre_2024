db9f1a8f4d8dff4e1edfef1d504b6c3894358c3a
Elias Maruca <elias.maruca.etu@univ-lille.fr>
2024-09-04 17:09:01 +0200

    Ajustement defense ennemis et classes

--- src/main/java/MobEnum.java
+++ src/main/java/MobEnum.java
@@ -1,19 +1,18 @@
 package main.java;
 
 public enum MobEnum {
-    
-    DRAGON(0.15,10,10, Monde.MONDE_2, 600, 100, 140, false, 500),
-    AMOGUS(0.15,10,10, Monde.MONDE_2, 700, 110, 150, false, 520),
-    ORC(0.15,10,10, Monde.MONDE_0, 210, 40, 40, false, 70),
-    SERPENT(0.15,10,10, Monde.MONDE_1, 250, 70, 80, false, 280),
-    VAMPIRE(0.15,10,10, Monde.MONDE_1, 350, 70, 90, false, 300),
-    SLIME(0.15,10,10, Monde.MONDE_0, 100, 20, 30, false, 50),
-    SQUELETTE(0.15,10,10, Monde.MONDE_3, 1300, 110, 150, false, 1500),
-    DEMON(0.15,10,10, Monde.MONDE_3, 1200, 120, 150, false, 1500),
-    SHREK(0.15,10,10, Monde.MONDE_2, 800, 120, 200, true, 1020),
-    KRAKEN(0.15,10,10, Monde.MONDE_1, 500, 100, 150, true, 520),
-    BIG_SLIME(0.15,10,10, Monde.MONDE_0, 380, 80, 50, true, 300),
-    REINE_DE_LA_FOURMILLIERE(0.15,10,10, Monde.MONDE_3, 2000, 170, 200, true, 0);
+    DRAGON(0.15,10,10, Monde.MONDE_2, 600, 50, 140, false, 500),
+    AMOGUS(0.15,10,10, Monde.MONDE_2, 700, 55, 150, false, 520),
+    ORC(0.15,10,10, Monde.MONDE_0, 210, 20, 40, false, 70),
+    SERPENT(0.15,10,10, Monde.MONDE_1, 250, 35, 80, false, 280),
+    VAMPIRE(0.15,10,10, Monde.MONDE_1, 350, 35, 90, false, 300),
+    SLIME(0.15,10,10, Monde.MONDE_0, 100, 10, 30, false, 50),
+    SQUELETTE(0.15,10,10, Monde.MONDE_3, 1300, 55, 150, false, 1500),
+    DEMON(0.15,10,10, Monde.MONDE_3, 1200, 60, 150, false, 1500),
+    SHREK(0.15,10,10, Monde.MONDE_2, 800, 60, 200, true, 1020),
+    KRAKEN(0.15,10,10, Monde.MONDE_1, 500, 50, 150, true, 520),
+    BIG_SLIME(0.15,10,10, Monde.MONDE_0, 380, 40, 50, true, 300),
+    REINE_DE_LA_FOURMILLIERE(0.15,10,10, Monde.MONDE_3, 2000, 85, 200, true, 0);
 
     private final double DROP;
     private final int DROPATK;
@@ -58,7 +57,6 @@ public enum MobEnum {
     public int getDROPDEF() {
         return DROPDEF;
     }
-    
 
     public Monde getMonde() {
         return MONDE;
