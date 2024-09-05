75a3f660714060ddfaf3e89b2d514a212ae8e752
Maxime Gosselin~ <maxime.gosselin.etu@univ-lille.fr>
2024-09-05 08:56:47 +0200

    gameOver v1.01

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -145,7 +145,7 @@ public class Game {
         UI.addLogs(mob.getNom() + " ⚔ " + degat + " ❤ " + joueur.getCategorie().getNom());
         joueur.setPv(joueur.getPv() - degat);
         if (joueur.getPv() <= 0) {
-            // GAME OVER
+            this.setState(GameState.GAME_OVER);
         }
     }
 
