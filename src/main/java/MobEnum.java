93be526a99850ab07255cb7e7c65a0e310a23447
elias maruca <elias.maruca.etu@univ-lille.fr>
2024-09-05 10:17:25 +0200

    Ajout du dernier boss + ajustement de la chance d'activer les compétences

--- src/main/java/MobEnum.java
+++ src/main/java/MobEnum.java
@@ -1,18 +1,20 @@
 
 
 public enum MobEnum {
-    DRAGON(0.15,10,10, Monde.MONDE_2, 600, 50, 140, false, 500, new CompetenceMob("Souffle du dragon",170,0,0,10,0,0,0.2)),
-    AMOGUS(0.15,10,10, Monde.MONDE_2, 700, 55, 150, false, 520, new CompetenceMob("Renforcement",0,20,0,0,0,0,0.3)),
-    ORC(0.15,10,10, Monde.MONDE_0, 210, 20, 40, false, 70, new CompetenceMob("Coup écrasant",60,0,0,0,0,0,0.15)),
-    SERPENT(0.15,10,10, Monde.MONDE_1, 250, 35, 80, false, 280, new CompetenceMob("Mue",0,25,-5,0,0,0,0.2)),
-    VAMPIRE(0.15,10,10, Monde.MONDE_1, 350, 35, 90, false, 300, new CompetenceMob("Saignée",30,0,0,0,0,70,0.3)),
-    SLIME(0.15,10,10, Monde.MONDE_0, 100, 10, 30, false, 50, new CompetenceMob("Gélaine collante",0,0,0,10,0,0,0.2)),
-    SQUELETTE(0.15,10,10, Monde.MONDE_3, 1300, 55, 150, false, 1500, new CompetenceMob("Armée des morts",180,0,0,10,0,0,0.4)),
-    DEMON(0.15,10,10, Monde.MONDE_3, 1200, 60, 150, false, 1500, new CompetenceMob("Flammes infernales",180,0,0,10,0,120,0.35)),
-    SHREK(0.15,10,10, Monde.MONDE_2, 800, 60, 200, true, 1020, new CompetenceMob("Cri effrayant", 150,0,0,0,30,0,0.2)),
-    KRAKEN(0.15,10,10, Monde.MONDE_1, 500, 50, 150, true, 520, new CompetenceMob("Tentation des profondeurs", 180,0,0,20,0,0,0.2)),
-    BIG_SLIME(0.15,10,10, Monde.MONDE_0, 380, 40, 50, true, 300, new CompetenceMob("Décharge d'acide",100,0,0,10,0,0,0.2)),
-    REINE_DE_LA_FOURMILLIERE(0.15,10,10, Monde.MONDE_3, 2000, 85, 200, true, 0, new CompetenceMob("Piège Mortel",300,0,0,0,20,0,0.1));
+    DRAGON(0.15,10,10, Monde.MONDE_2, 600, 50, 140, false, 500, new CompetenceMob("Souffle du dragon",170,0,0,10,0,0,0.3)),
+    AMOGUS(0.15,10,10, Monde.MONDE_2, 700, 55, 150, false, 520, new CompetenceMob("Renforcement",0,20,0,0,0,0,0.4)),
+    ORC(0.15,10,10, Monde.MONDE_0, 210, 20, 40, false, 70, new CompetenceMob("Coup écrasant",60,0,0,0,0,0,0.3)),
+    SERPENT(0.15,10,10, Monde.MONDE_1, 250, 35, 80, false, 280, new CompetenceMob("Mue",0,25,-5,0,0,0,0.4)),
+    VAMPIRE(0.15,10,10, Monde.MONDE_1, 350, 35, 90, false, 300, new CompetenceMob("Saignée",30,0,0,0,0,70,0.4)),
+    SLIME(0.15,10,10, Monde.MONDE_0, 100, 10, 30, false, 50, new CompetenceMob("Gélaine collante",0,0,0,10,0,0,0.4)),
+    SQUELETTE(0.15,10,10, Monde.MONDE_3, 1300, 55, 150, false, 1500, new CompetenceMob("Armée des morts",180,0,0,10,0,0,0.5)),
+    DEMON(0.15,10,10, Monde.MONDE_3, 1200, 60, 150, false, 1500, new CompetenceMob("Flammes infernales",180,0,0,10,0,120,0.5)),
+    SHREK(0.15,10,10, Monde.MONDE_2, 800, 60, 200, true, 1020, new CompetenceMob("Cri effrayant", 150,0,0,0,30,0,0.3)),
+    KRAKEN(0.15,10,10, Monde.MONDE_1, 500, 50, 150, true, 520, new CompetenceMob("Tentation des profondeurs", 180,0,0,20,0,0,0.3)),
+    BIG_SLIME(0.15,10,10, Monde.MONDE_0, 380, 40, 50, true, 300, new CompetenceMob("Décharge d'acide",100,0,0,10,0,0,0.35)),
+    REINE_DE_LA_FOURMILLIERE(0.15,10,10, Monde.MONDE_3, 2000, 85, 200, true, 0, new CompetenceMob("Piège Mortel",300,0,0,0,20,0,0.35)),
+    MERUEM(0.15,10,10, Monde.MONDE_4, 3500, 100, 250, true, 0, new CompetenceMob("Force immense",400,30,0,0,0,0,0.2));
+    // Attaque de meruem = "Meruem donne un coup de queue, inflige 400 de dégat et augmente son attaque de 30"
 
     private final double DROP;
     private final int DROPATK;
